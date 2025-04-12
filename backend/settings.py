bccb3d000267ba62d0b5d4ef1c388626f4c41a43
skapir <spereznt@gmail.com>
2025-04-11 21:14:38 -0500

    actualizando estilos y configuración Tailwind

--- backend/settings.py
+++ backend/settings.py
@@ -160,7 +160,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend', 'src'),   # ✅ Para el CSS y JS
+    # os.path.join(BASE_DIR, 'frontend', 'src'),   # ✅ Para el CSS y JS
     os.path.join(BASE_DIR, 'frontend', 'static'),  # ✅ Para imágenes y otros archivos estáticos
 ]
 
